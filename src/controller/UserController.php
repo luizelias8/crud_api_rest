ae56684e2db5c6994b70ff2a0f4f468e5a034c96
Luiz Elias <luizelias8@gmail.com>
2021-02-01 18:14:24 -0300

    Criando a classe UserController. Definindo as assinaturas dos métodos, que implementam a interface IController.

---
+++ src/controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Webslesson\Controller;
+
+use Webslesson\Controller\Controller;
+use Webslesson\Controller\IController;
+
+class UserController extends Controller implements IController
+{
+    public function get($id = null)
+    {
+
+    }
+    
+    public function post()
+    {
+
+    }
+
+    public function put($id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+        
+    }
+}
