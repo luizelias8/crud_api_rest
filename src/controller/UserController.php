d287aae9af8b9438fb810dccedc4e36fdb3dd772
Luiz Elias <luizelias8@gmail.com>
2021-02-02 20:52:04 -0300

    Implementando o método post da classe UserController. Criando e implementando o método create da classe UserRepository.

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -5,6 +5,8 @@ namespace Webslesson\Controller;
 use Webslesson\Controller\Controller;
 use Webslesson\Controller\IController;
 use Webslesson\Model\Repository\UserRepository;
+use Webslesson\Model\Entity\User;
+use Webslesson\Model\Service\UserService;
 
 class UserController extends Controller implements IController
 {
@@ -17,7 +19,28 @@ class UserController extends Controller implements IController
     
     public function post()
     {
+        $input = $this->input();
 
+        $user = new User();
+
+        $user->setName($input->name);
+        $user->setContactNo($input->contact_no);
+        $user->setEmail($input->email);
+        $user->setPassword($input->password);
+        $user->setProfile($input->profile);
+        $user->setType($input->type);
+        $user->setCreatedOn($input->created_on);
+        $user->setStatus($input->status);
+
+        $userService = new UserService();
+
+        if ($userService->validate($user)) {
+
+            $userRepository = new UserRepository();
+
+            return $userRepository->create($user);
+
+        }
     }
 
     public function put($id)
--- src/model/repository/UserRepository.php
+++ src/model/repository/UserRepository.php
@@ -4,6 +4,7 @@ namespace Webslesson\Model\Repository;
 
 use Webslesson\Model\Repository\Repository;
 use PDO;
+use Webslesson\Model\Entity\User;
 
 class UserRepository extends Repository
 {
@@ -18,5 +19,24 @@ class UserRepository extends Repository
 			return $this->select("SELECT * FROM user");
 
 		}		
+	}
+
+	public function create(User $user)
+	{
+		$stmt = $this->query("
+			INSERT INTO user(name, contact_no, email, password, profile, type, status, created_on) 
+			VALUES(:name, :contact_no, :email, :password, :profile, :type, :status, :created_on)
+		", [
+			":name" => $user->getName(), 
+			":contact_no" => $user->getContactNo(), 
+			":email" => $user->getEmail(), 
+			":password" => $user->getPassword(), 
+			":profile" => $user->getProfile(), 
+			":type" => $user->getType(), 
+			":status" => $user->getStatus(), 
+			":created_on" => $user->getCreatedOn()
+		]);
+		
+		return $stmt->rowCount();
 	}
 }
