569e0751397de67b1a0a96292f5f074e3ebcaf06
Luiz Elias <luizelias8@gmail.com>
2021-02-02 15:12:23 -0300

    Criando o método read da classe UserRepository e criando a implementação do método get da classe UserController

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -4,12 +4,15 @@ namespace Webslesson\Controller;
 
 use Webslesson\Controller\Controller;
 use Webslesson\Controller\IController;
+use Webslesson\Model\Repository\UserRepository;
 
 class UserController extends Controller implements IController
 {
     public function get($id = null)
     {
+        $userRepository = new UserRepository();
 
+        return $userRepository->read($id);
     }
     
     public function post()
--- src/model/repository/UserRepository.php
+++ src/model/repository/UserRepository.php
@@ -7,5 +7,16 @@ use PDO;
 
 class UserRepository extends Repository
 {
+	public function read($id = null)
+	{
+		if ($id) {
 
+			return $this->select("SELECT * FROM user WHERE :id = id", [":id" => $id]);
+
+		} else {
+
+			return $this->select("SELECT * FROM user");
+
+		}		
+	}
 }
