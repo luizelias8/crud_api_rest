ddb3eecccc221e55177642397d82a624a131055d
Luiz Elias <luizelias8@gmail.com>
2021-02-02 15:01:47 -0300

    Criando métodos auxiliares da classe base Repository: setParam, setParams, query e select.

--- src/model/repository/Repository.php
+++ src/model/repository/Repository.php
@@ -7,5 +7,35 @@ use PDO;
 
 class Repository
 {
-	
+	private function setParam($statment, $key, $value)
+	{
+		$statment->bindParam($key, $value);
+	}
+
+	private function setParams($statment, $parameters = [])
+	{
+		foreach ($parameters as $key => $value) {
+
+			$this->setParam($statment, $key, $value);
+
+		}
+	}
+
+	public function query($rawQuery, $params = [])
+	{
+		$stmt = Connect::getInstance()->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt;
+	}
+
+	public function select($rawQuery, $params = [])
+	{
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_OBJ);
+	}
 }
